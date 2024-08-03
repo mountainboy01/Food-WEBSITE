541ac4a5efb04824ed1ca2afde230c9a07a685dd
mountainboy01 <dharmendrachauhan977@gmail.com>
2024-08-03 14:16:57 +0530

    food-website

---
+++ js/main.js
@@ -0,0 +1,60 @@
+/***************************** NAVBAR JS **********************************/
+
+const navbar= document.querySelector("#navbar");
+const toggleIcon = document.querySelector("#toggle-icon");
+
+toggleIcon.addEventListener("click",()=>{
+      navbar.classList.toggle("active");
+      toggleIcon.classList.toggle("fa-times");
+})
+
+/***************************** NAVBAR JS **********************************/
+
+
+
+/***************************** OUR MENU SWIPER JS **********************************/
+var swiper = new Swiper(".mySwiper", {
+      slidesPerView: "auto",
+      spaceBetween: 20,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: false,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+      },
+    });
+
+
+/***************************** OUR MENU SWIPER JS **********************************/
+
+
+/***************************** SCROLL TOP JS **********************************/
+const scrollTop = document.querySelector("#scrollTop");
+window.addEventListener("scroll",()=>{
+	if(window.pageYOffset>300){
+	scrollTop.classList.add("active");
+     }
+
+     else{
+     	scrollTop.classList.remove("active");
+     }
+})
+
+scrollTop.addEventListener("click",()=>{
+	window.scrollTo(0 ,0);
+})
+
+
+/***************************** SCROLL TOP JS **********************************/
